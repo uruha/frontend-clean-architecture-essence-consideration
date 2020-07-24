205ab4265399b4d1ba8ed04f5766db06334b1ee1
uruha <kochmusica@gmail.com>
2020-07-24 15:56:23 +0900

    update: fixed fmt

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,14 @@ import { useState, useContext } from 'react';
 import { DIContainerContext } from '~/web/view/context';
 
 const App: React.FC = () => {
-  const { cradle } = useContext(DIContainerContext);
+  const { createTodo } = useContext(DIContainerContext).cradle;
   const [title, setTitle] = useState('');
   const [detail, setDetail] = useState('');
 
   const handleSubmit = async () => {
     const input = { title, detail };
     try {
-      const { todo } = await cradle.createTodo(input);
+      const { todo } = await createTodo(input);
       console.log(todo);
 
       setTitle('');
@@ -22,9 +22,11 @@ const App: React.FC = () => {
 
   return (
     <>
+      <h1>Todo application</h1>
       <div>
         <ul>
           <li>
+            Todo title:
             <input
               type="text"
               value={title}
@@ -32,6 +34,7 @@ const App: React.FC = () => {
             />
           </li>
           <li>
+            Todo detail:
             <input
               type="text"
               value={detail}
