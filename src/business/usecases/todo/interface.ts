f4569b6d25bc73fdaae746af1450be39d959b3bb
uruha <kochmusica@gmail.com>
2020-09-05 15:37:56 +0900

    add: get todo usecases

--- src/business/usecases/todo/interface.ts
+++ src/business/usecases/todo/interface.ts
@@ -1,9 +1,11 @@
-import { ITodo } from '~/business/entities';
+import { ITodo, ITodoList } from '~/business/entities';
 import { IUseCase } from '~/business/usecases/interface';
 
 /** Data Access */
 export interface IDataAccess {
   createTodo(title: ITodo['title'], detail: ITodo['detail']): Promise<ITodo>;
+  getTodoList(): Promise<ITodoList>;
+  getTodo(id: ITodo['id']): Promise<ITodo>;
 }
 
 /** I/O */
@@ -11,13 +13,26 @@ export interface ICreateTodoInput {
   title: ITodo['title'];
   detail?: ITodo['detail'];
 }
+export interface IGetTodoInput {
+  id: ITodo['id'];
+}
 
 export interface ICreateTodoOutput {
   todo: ITodo;
 }
+export interface IGetTodoListOutput {
+  todoList: ITodoList;
+}
+export interface IGetTodoOutput {
+  todo: ITodo;
+}
 
 export type ICreateTodo = IUseCase<ICreateTodoInput, ICreateTodoOutput>;
+export type IGetTodoList = IUseCase<null, IGetTodoListOutput>;
+export type IGetTodo = IUseCase<IGetTodoInput, IGetTodoOutput>;
 
 export interface ITodoUsecase {
   createTodo: ICreateTodo;
+  getTodoList: IGetTodoList;
+  getTodo: IGetTodo;
 }
